41efe4f5f37587aa3ca765df0f49d4bc903a773b
Timothy Aveni <timothy@tacosareawesome.com>
2014-11-21 16:07:25 -0500

    Update other languages

--- gen/net/syntaxblitz/plucklock/R.java
+++ gen/net/syntaxblitz/plucklock/R.java
@@ -26,10 +26,12 @@ public final class R {
     }
     public static final class string {
         public static final int app_name=0x7f050000;
-        public static final int failed_to_lock=0x7f050007;
+        public static final int prefs_enable=0x7f050007;
         public static final int prefs_enable_device_admin=0x7f050004;
         public static final int prefs_enable_device_admin_description=0x7f050005;
         public static final int prefs_enabled=0x7f050001;
+        /**  TODO: UNCHECKED 
+         */
         public static final int prefs_threshold=0x7f050002;
         public static final int prefs_threshold_description=0x7f050003;
         public static final int too_low=0x7f050006;
